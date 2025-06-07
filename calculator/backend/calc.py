672d016d59530b641183ccaf69f6c7e207e54621
Vaagish Kumar Singh <vaagish1124@gmail.com>
2025-06-07 12:36:21 +0000

    added return result

--- calculator/backend/calc.py
+++ calculator/backend/calc.py
@@ -2,28 +2,34 @@
 def add(num1, num2):
     result = num1 + num2
     print(result)
+    return result
     
 
 def sub(num1, num2):
     result = num1-num2
     print(result)
+    return result
     
 
 def mult(num1, num2):
     result = num1*num2
     print(result)
+    return result
     
 
 def div(num1, num2):
     ## do not divide by zero
     if num2 == 0:
         print("Syntax Error")
+        return float('inf')
     else:   
         result = num1/num2 
         print(result)
+        return result   
 
 def calculate(num1, num2, operator):
-    
+    print(num1,num2, operator)
+    print(type(num1), type(num2), type(operator))
     if operator == "+":
         return add(num1, num2)
     elif operator == "-":
@@ -36,11 +42,11 @@ def calculate(num1, num2, operator):
         print("Pls give a valid operator")
         return "error"
 
-if __name__ == "__main__":
-    num1=float(input("Enter first number: "))
-    num2=float(input("Enter second number: "))
-    operator = input("Enter the operator(+,-,*,/): ")
+#if __name__ == "__main__":
+  # num1=float(input("Enter first number: "))
+   # num2=float(input("Enter second number: "))
+   # operator = input("Enter the operator(+,-,*,/): ")
 
-    calculate(add, sub, mult, div, num1, num2, operator)
+   # calculate(add, sub, mult, div, num1, num2, operator)
 
     
